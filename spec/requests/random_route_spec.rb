8d3e7c2851dcc9cd7f58b0865791f4f0159b4907
Matt Casperson <mc.casperson@gmail.com>
2022-01-21 12:25:01 -0800

    add random spec

---
+++ spec/requests/random_route_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+describe 'get a random animal route', :type => :request do
+  let!(:animals) { FactoryBot.create_list(:animal, 20) }
+
+  before { get '/animals/random'}
+
+  it 'returns a single animal' do
+    expect(JSON.parse(response.body).size).to eq(6)
+  end
+
+  it 'returns status code 200' do
+    expect(response).to have_http_status(200)
+  end
+
+end
