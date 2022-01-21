024ee5453df5ba2a072007025dfc90ceadea6bea
Matt Casperson <mc.casperson@gmail.com>
2022-01-21 12:18:46 -0800

    pass DELETE spec

---
+++ spec/requests/delete_route_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+describe "delete an animal (from the database) route", :type => :request do
+  let!(:animals) { FactoryBot.create_list(:animal, 20) }
+
+  before { delete "/animals/#{Animal.first.id}"}
+
+  it 'returns status code 200' do
+    expect(response).to have_http_status(200)
+  end
+
+  it 'returns a delete success message' do
+    expect(JSON.parse(response.body)['message']).to eq("This animal has been deleted (from the database) successfully.")
+  end
+end
