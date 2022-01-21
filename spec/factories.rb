323919d34b12c7d30fb33f249705c72df71a8871
Matt Casperson <mc.casperson@gmail.com>
2022-01-21 11:21:12 -0800

    fix factory, pass GET specs

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,12 +1,7 @@
 FactoryBot.define do
-  factory(:cat) do
+  factory(:animal) do
     name {Faker::JapaneseMedia::Naruto.character}
     species {'cat'}
     age {Faker::Number.between(from: 1, to: 18)}
-  end
-  factory(:dog) do
-    name {Faker::JapaneseMedia::DragonBall.character}
-    species {'dog'}
-    age {Faker::Number.between(from: 1, to: 15)}
   end
 end
---
+++ spec/requests/get_animals_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+describe "get all pets route", :type => :request do
+  let!(:animals) { FactoryBot.create_list(:animal, 9) }
+
+  it 'returns all animals' do
+    get '/animals'
+    expect(JSON.parse(response.body).size).to eq(9)
+  end
+
+  it 'returns the first 5 animals' do
+    get '/animals?page=1'
+    expect(JSON.parse(response.body).size).to eq(5)
+  end
+
+  it 'returns the last 4 animals' do
+    get '/animals?page=2'
+    expect(JSON.parse(response.body).size).to eq(4)
+  end
+
+  it 'returns status code 200' do
+    get '/animals'
+    expect(response).to have_http_status(:success)
+  end
+
+end
